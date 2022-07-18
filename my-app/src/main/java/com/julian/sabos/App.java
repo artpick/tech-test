78764cf410897bebbba4a0abeda4b1a4128a8d7a
Julian Sabos <julian.sabos.partner@decathlon.com>
2022-07-18 15:14:03 +0200

    Intialize the tech tech

---
+++ my-app/src/main/java/com/julian/sabos/App.java
@@ -0,0 +1,13 @@
+package com.julian.sabos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
